754da99372b8f56ae4dac3a2ffbd318052c28ca3
ccyberbear <duycazami92012001@gmail.com>
2022-04-24 18:34:27 +0700

    fixUpdate

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -4,7 +4,7 @@ const PRODUCT = require("../controllers/productController");
 const mongoUpload = require("../middlewares/uploadMongo");
 
 router.post("/create", mongoUpload.single("image"), PRODUCT.createProduct);
-router.put("/update/:id", mongoUpload.single("image"), PRODUCT.updateProduct);
+router.post("/update/:id", mongoUpload.single("image"), PRODUCT.updateProduct);
 router.delete("/delete/:id", PRODUCT.deleteProduct);
 router.get("/all-product", PRODUCT.getAll);
 
